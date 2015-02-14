1356dc3f9203d7ca1e6a0ec5c0fc80afafa83a3b
millionhari <mikemhari@gmail.com>
2015-02-14 12:29:01 -0800

    implement drag function

--- watchout.js
+++ watchout.js
@@ -27,13 +27,14 @@ var player = {
   fill: 'white',
   strokeColor: "white",
   strokeWidth: .4,
-  x: 450,
-  y: 250,
+  x: gameBoard.width/2,
+  y: gameBoard.height/2,
   angle: 0,
-  path: 'M31.501,1.098c-0.782-1.125-2.375-1.416-4.249-0.716L2.583,9.46C1.2,9.97-0.182,10.647,0.02,11.809c0.209,1.154,1.744,1.311,3.215,1.311h11.256c2.42,0,4.388,1.968,4.388,4.386V28.76c0,0.966,0,3.231,1.517,3.236c0.841,0,1.458-0.749,2.133-2.586l9.086-24.669C32.307,2.854,31.94,1.717,31.501,1.098zM29.851,4.093L20.77,28.762c-0.009,0.016-0.009,0.026-0.018,0.04c0-0.014,0-0.029,0-0.042V17.505c0-3.455-2.805-6.265-6.262-6.265H3.235c-0.018,0-0.035,0-0.062,0c0.023-0.005,0.036-0.013,0.059-0.021l24.665-9.078c0.993-0.366,1.808-0.351,2.067,0.031C30.209,2.518,30.168,3.237,29.851,4.093z',
+  path: 'M24.861,4.485C23.62,2.416,20.556,0,16.004,0c-4.554,0-7.62,2.416-8.862,4.485C4.146,9.474,2.001,31.99,2,32c2.33-0.003,2.332-2,4.665-2c2.334,0,2.334,2,4.668,2s2.334-2,4.669-2h0.002h0.002c2.334,0,2.334,2,4.667,2s2.333-2,4.665-2s2.334,1.997,4.662,2C29.999,31.99,27.854,9.474,24.861,4.485zM10.004,15c-1.104,0-2-0.896-2-2s0.896-2,2-2s2,0.896,2,2S11.108,15,10.004,15zM22.004,15c-1.104,0-2-0.896-2-2s0.896-2,2-2s2,0.896,2,2S23.108,15,22.004,15z',
   makePlayer: function(){
     var playerD3 = gameBoardD3
       .append('svg:path')
+      .attr('class', 'hero')
       .attr('fill', this.fill)
       .attr('d', this.path)
       .attr('stroke', this.strokeColor)
@@ -58,6 +59,7 @@ var enemy = {
   makeEnemy: function(){
     var enemyD3 = gameBoardD3
       .append('svg:circle')
+      .attr('class', 'enemy')
       .attr('cx', this.cx)
       .attr('cy', this.cy)
       .attr('fill', this.fill)
@@ -81,7 +83,7 @@ var enemy = {
   move: function(){
     gameBoardD3.selectAll('circle')
       .data(this.randomPosition())
-      .transition().duration(1500)
+      .transition().duration(2000)
       .attr('r', 10)
       .attr('cx', function(x){return x[0]})
       .attr('cy', function(y){return y[1]})
@@ -97,25 +99,41 @@ var gameBoardD3 =
   .style('width', gameBoard.width+'px')
   .style('height', gameBoard.height+'px')
 
+var drag = d3.behavior.drag()
+  .on("drag", function(d,i) {
+    console.log("clicked")
+    console.log(player.x, player.y)
+    player.x += d3.event.dx
+    player.y += d3.event.dy
+    // might be a prob w collision
+    d3.select(this).attr("transform", function(){
+        return "translate(" + [ player.x,player.y ] + ")"
+    })
+  });
+
 player.makePlayer()
 enemy.generate()
 setInterval(function(){
-  enemy.move()
-},2000);
-// var move = function(){
-//   d3.select('gameBoardD3').select("circle")
-//     .data([Math.random()*this.height])
-//     .transition().duration(1000)
-//     .attr("this.cy",  function(h){
-//       return h
-//     })
-// }
+  enemy.move();
+},2500);
 
+d3.select(".hero")
+  .call(drag);
 
+var data = d3.selectAll(".enemy")
+console.log(data);
 
+var collision = function(){
+var distance = Math.sqrt(Math.pow(player.x-enemy.cx(), 2) + Math.pow(player.y-enemy.cy(), 2));
+  console.log('playerx: '+ player.x)
+  console.log('playery: '+ player.y)
+  console.log('enemycx: '+ enemy.cx())
+  console.log('enemycy: '+ enemy.cy())
 
+  if(distance < 100){
+    console.log("HIT")
+  } else {
+    console.log("MISS")
+  }
 
-
-
-
-
+}
